e2f795a45840c2c32e895736be8c9de12a9965a7
liars <lars27399@gmail.com>
2019-07-03 12:42:36 +0700

    f

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const mongoose = require("mongoose");
 const config = require("./config/key");
 
+console.log(config.MONGO_URI);
+
 mongoose.connect(config.MONGO_URI, {useNewUrlParser: true});
 
 // Import routes
